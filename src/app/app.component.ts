4ac63b0100bc93c843fc1d1d51431ec4751840ea
Nedimm23 <dzibra_23@hotmail.com>
2019-02-26 19:47:57 +0100

    new components for my app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   //styleUrls: ['./app.component.css']
   styles: [`
-  h3 {
+    h3 {
     color: dodgerblue;
   }
   `]
